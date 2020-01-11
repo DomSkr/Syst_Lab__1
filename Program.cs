9fd268d02f5b66488930b5d357602136f16a1385
Kluzka <ytworek@gmail.com>
2020-01-11 14:43:54 +0100

    .net app start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Systemy_kontroli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
