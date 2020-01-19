f847f78f7cd835f4dbe4042f89889ac45334af58
Kluzka <ytworek@gmail.com>
2020-01-19 22:32:01 +0100

    start develop

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Systemy_kontroli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LoginUser();
+        }
+        static void LoginUser(){
+            Console.WriteLine("Podaj login");
+            Console.ReadLine();
+            Console.WriteLine("Podaj hasło");
+
+            // if!(ligin in logins or password in passwords){
+            //     "Niepoprawny login lub hasło"
+            // }
+
         }
     }
 }
